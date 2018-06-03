8b4a1b75b7e03ffd9b16847644fa12ced6a6658d
blaise <exploit@o2.pl>
2018-06-03 16:51:17 +0200

    1st commit

---
+++ src/main/java/pl/joke/controllers/JokeController.java
@@ -0,0 +1,29 @@
+package pl.joke.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.joke.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
+
+
+git commit -m "first commit"
+git remote add origin https://github.com/BlaiseDee/jokeApp.git
+git push -u origin master
---
+++ src/main/java/pl/joke/services/JokeService.java
@@ -0,0 +1,5 @@
+package pl.joke.services;
+
+public interface JokeService {
+    String getJoke();
+}
